a8573138bb03bbf3c616c3cbdbab9e0f79ec1640
PachV <95147277+PachV@users.noreply.github.com>
2022-03-31 01:12:17 -0400

    Update lister.py

--- lister.py
+++ lister.py
@@ -1,10 +1,3 @@
-### This is a lister for movies(or databses like IMDb, MyAnimeList, VNDB, etc) you have watched and their rating. WIP
-# To do list:
-    # Add QT GUI and a image saving/scraping function(probably need to learn C++) 
-    # Add plan-to watch list-
-    #  plan-to watch list will be in another file (json) as a list datatype, if the user writes the key to the list.json that is
-    #  already in the to plan-to watch list file, the list file's index (name) will be deleted  
- 
 import json
 import read_algo as readalgo
 import write_algo as writealgo
